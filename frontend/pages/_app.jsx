0d9087dc656021466dcdaa0ae4f622265f35e9df
Nicholas Singh <nicholas1singh@gmail.com>
2022-01-17 10:13:20 -0500

    added title

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-
 import "normalize.css";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
+
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Journaling App</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
